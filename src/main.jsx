60aaf5f24772f5366d023b5317c7fa0796ebaed5
Burhan-sheikh <epithet02@gmail.com>
2025-11-23 15:55:58 +0530

    Updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import './styles/globals.css'
 
-// Import service worker registration
 import { registerSW } from 'virtual:pwa-register'
 
-// Register service worker
 if ('serviceWorker' in navigator) {
   registerSW({
-    onNeedRefresh() {
-      console.log('New content available, please refresh.')
-    },
-    onOfflineReady() {
-      console.log('App ready to work offline')
-    },
+    onNeedRefresh() {},
+    onOfflineReady() {},
   })
 }
 
